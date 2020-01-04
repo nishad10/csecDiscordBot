61edbf983bcfba1d93ba0453b4bf2be1ade03c6e
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-01-04 15:58:42 -0600

    Added functions and server api call to make sure commandds work

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://utacsecapi.herokuapp.com'
+// 'https://utacsecapi.herokuapp.com' || 'http://localhost:8000'
 
 export const getEvents = () => {
   return axios
@@ -11,4 +12,36 @@ export const getEvents = () => {
     .catch(error => {
       console.log(error)
     })
+}
+export const getEventID = eventName => {
+  return axios
+    .post(`/getEventInfo`, { eventName })
+    .then(res => {
+      return res
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const getUser = discordID => {
+  return axios
+    .post(`/discordUser`, { discordID })
+    .then(res => {
+      return res
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+export const doRsvp = (eventID, userID) => {
+  return axios
+    .post(`/rsvp`, {
+      eventID,
+      userID,
+    })
+    .then(res => {
+      return res
+    })
+    .catch(err => err)
 }
